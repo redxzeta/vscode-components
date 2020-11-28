7df70839a31739331773c8ac0edadd71959677cb
redxzeta <xzetanzero@gmail.com>
2020-11-28 10:45:11 -0800

    adding structure for files part

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch,useState, SetStateAction } from 'react';
+import React, { Dispatch, useState, SetStateAction } from 'react';
 import MonacoEditor from './monacoEditor';
 // import dynamic from "next/dynamic";
 // let MonacoEditor = dynamic(() => import("./monacoEditor"), { ssr: false });
@@ -15,85 +15,123 @@ export type editorProps = {
   onSuccess?: Dispatch<SetStateAction<number>>;
   onFailure?: Function;
   submissionCount?: number;
-
 };
 export type sideBarProps = {
-  button?: boolean,
+  button?: boolean;
   // showSection?: Function
-  showSection: () => void
-}
-
-
+  showSection: () => void;
+};
 
 const useStyles = makeStyles({
-  gridBox:{
-    height:"auto",
-    minHeight:"500px"
+  gridBox: {
+    height: 'auto',
+    minHeight: '500px',
   },
-  sideBar:{
+  sideBar: {
     backgroundColor: '#2d3233',
-    width:"100%",
-    height:"inherit",
-    padding:"10px 0"
+    width: '100%',
+    height: 'inherit',
+    padding: '10px 5px',
   },
-  button:{
-    color:"white",
-    width:"100%",
-    background:"#5e696b",
-    fontSize:"8px",
-    margin:'auto',
+  button: {
+    color: 'white',
+    width: '100%',
+    background: '#5e696b',
+    fontSize: '8px',
+    margin: 'auto',
     // alignItems:"center"
   },
-  buttonText:{
-    
-  }
-
-})
+  section: {
+    width: '100%',
+  },
+  components: {
+    backgroundColor: '#2d3233',
+    width: '100%',
+    height: 'auto',
+  },
+  componentsSection: {
+    width: '100%',
+    marginLeft: '15px',
+  },
+  componentsButton: {
+    width: '100%',
+    height: '10px',
+  },
+});
+const sampleData = [
+  {
+    _id: '_src',
+    name: 'src',
+    type: 'folder',
+    files: [
+      {
+        id: '_index',
+        name: 'index.tsx',
+        type: 'tsx',
+      },
+    ],
+  },
+  {
+    _id: '_package.json',
+    name: 'package.json',
+    type: 'json',
+  },
+];
 export default function Editor({
   id,
   modelsInfo,
   onSuccess,
   onFailure,
   submissionCount,
- 
 }: editorProps) {
-  const classes = useStyles()
- const [active, setActive] = useState(true);
- const showSection = (): void=>{
-   setActive(!active)
-   console.log(active)
- }
+  const classes = useStyles();
+  const [active, setActive] = useState(true);
+  const showSection = (): void => {
+    setActive(!active);
+    console.log(active);
+  };
 
   return (
     <DndProvider backend={HTML5Backend}>
-    <Grid container spacing ={0} className={classes.gridBox}>
-    {/* showSection={showSection()} */}
-    <Grid className={classes.sideBar} item md={3} xs={12}><Sidebar button={active} showSection={showSection} /></Grid>
-    <Grid item md={9} xs={12} >
-      <EditorProvider>
-        <MonacoEditor
-          submissionCount={submissionCount}
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          id={id}
-          modelsInfo={modelsInfo}
-        />
-      </EditorProvider>
-      </Grid>
+      <Grid container spacing={0} className={classes.gridBox}>
+        {/* showSection={showSection()} */}
+        <Grid className={classes.sideBar} item md={3} xs={12}>
+          <Sidebar button={active} showSection={showSection} />
+        </Grid>
+        <Grid item md={9} xs={12}>
+          <EditorProvider>
+            <MonacoEditor
+              submissionCount={submissionCount}
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+              id={id}
+              modelsInfo={modelsInfo}
+            />
+          </EditorProvider>
+        </Grid>
       </Grid>
     </DndProvider>
   );
 }
 
-const Sidebar = ({button,showSection}:sideBarProps)=>{
-const classes = useStyles()
-  return(
-    <Box>
-    <Button onClick={showSection} className={classes.button}>
-
-  { button ? <ChevronRightIcon  fontSize="small"/> : <ExpandMoreIcon fontSize="small"/>  }   <Typography variant="body2">VSCODE-COMPONENTS
-      </Typography>  <div style={{flexGrow:1}}/>
-    </Button>
+const Sidebar = ({ button, showSection }: sideBarProps) => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.section}>
+      <Button onClick={showSection} className={classes.button}>
+        {button ? (
+          <ChevronRightIcon fontSize="small" />
+        ) : (
+          <ExpandMoreIcon fontSize="small" />
+        )}{' '}
+        <Typography variant="body2">VSCODE-COMPONENTS</Typography>{' '}
+        <div style={{ flexGrow: 1 }} />
+      </Button>
+      <Box className={classes.components}>
+        <Box className={classes.componentsSection}>
+          <Typography variant="h5">TEST</Typography>
+        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
